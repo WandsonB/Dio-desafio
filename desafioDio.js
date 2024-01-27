067960886917411f1cf7ebd55242e5e174b3c902
WandsonB <wandson.study@gmail.com>
2024-01-27 15:22:26 -0300

    Criação do Desafio do Herois

--- desafioDio.js
+++ desafioDio.js
@@ -1,4 +1,33 @@
-console.log("Hellow new world")
+console.log("Hellow, new world")
 
-let nome = "Wandson"
-console.log(nome)
+let nomeHeroi = "Wandson"
+
+let qtdXP = 999
+let classHero = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Readiante"]
+
+
+if (qtdXP <= 1000){ 
+    console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[0]}`)
+    
+} else if (qtdXP >= 1001 && qtdXP <+2000){ 
+    console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[1]}`)
+    
+} else if (qtdXP >= 2001 && qtdXP <+3000){ 
+    console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[2]}`)
+    
+} else if (qtdXP >= 3001 && qtdXP <+4000){ 
+    console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[3]}`)
+    
+} else if (qtdXP >= 4001 && qtdXP <+5000){ 
+    console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[4]}`)
+    
+} else if (qtdXP >= 5001 && qtdXP <+6000){ 
+    console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[5]}`)
+    
+} else if (qtdXP >= 6001 && qtdXP <+7000){ 
+    console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[6]}`)
+    
+} else if (qtdXP > 7001){ 
+    console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[7]}`)
+    
+}
