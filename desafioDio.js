1d64289e4da1def5ab75cecc06470a3fa53210b9
WandsonB <wandson.study@gmail.com>
2024-02-04 23:50:45 -0300

    teste switch case

--- desafioDio.js
+++ desafioDio.js
@@ -1,33 +1,66 @@
 console.log("Hellow, new world")
 
-let nomeHeroi = "Wandson"
+let nomeHeroi = "Besercker"
 
-let qtdXP = 999
+let qtdXP = 5999
 let classHero = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Readiante"]
-
 
 if (qtdXP <= 1000){ 
     console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[0]}`)
     
-} else if (qtdXP >= 1001 && qtdXP <+2000){ 
+}else if(qtdXP >= 1001 && qtdXP <+2000){ 
     console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[1]}`)
     
-} else if (qtdXP >= 2001 && qtdXP <+3000){ 
+}else if(qtdXP >= 2001 && qtdXP <+3000){ 
     console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[2]}`)
     
-} else if (qtdXP >= 3001 && qtdXP <+4000){ 
+}else if(qtdXP >= 3001 && qtdXP <+4000){ 
     console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[3]}`)
     
-} else if (qtdXP >= 4001 && qtdXP <+5000){ 
+}else if(qtdXP >= 4001 && qtdXP <+5000){ 
     console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[4]}`)
     
-} else if (qtdXP >= 5001 && qtdXP <+6000){ 
+}else if(qtdXP >= 5001 && qtdXP <+6000){ 
     console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[5]}`)
     
-} else if (qtdXP >= 6001 && qtdXP <+7000){ 
+}else if(qtdXP >= 6001 && qtdXP <+7000){ 
     console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[6]}`)
     
-} else if (qtdXP > 7001){ 
+}else if(qtdXP > 7001){ 
     console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[7]}`)
     
 }
+
+// switch (qtdXP){
+//      case qtdXP < 1000:
+//          console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[0]}`)
+//          break
+
+//      case (qtdXP >= 1001 && qtdXP <+2000): 
+//          console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[1]}`)
+//          break
+        
+//      case (qtdXP >= 2001 && qtdXP <+3000): 
+//          console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[2]}`)
+//          break
+        
+//      case (qtdXP >= 3001 && qtdXP <+4000): 
+//          console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[3]}`)
+//          break
+        
+//      case (qtdXP >= 4001 && qtdXP <+5000): 
+//          console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[4]}`)
+//          break
+        
+//      case (qtdXP >= 5001 && qtdXP <+6000): 
+//          console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[5]}`)
+//          break
+        
+//      case (qtdXP >= 6001 && qtdXP <+7000): 
+//          console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[6]}`)
+//          break
+        
+//      case (qtdXP > 7001): 
+//          console.log(`O heroi de nome: ${nomeHeroi} está na classsificação ${classHero[7]}`)
+//          break      
+// }
